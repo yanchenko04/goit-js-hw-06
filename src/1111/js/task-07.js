14551ed4777bc85aaee9a47dfb823964d0bd6b05
yanchenko04 <yanchenko0405@gmail.com>
2022-09-07 21:35:52 +0300

    21

--- src/1111/js/task-07.js
+++ src/1111/js/task-07.js
@@ -1,5 +1,7 @@
 const input = document.querySelector('#font-size-control');
 const span = document.querySelector('#text');
+span.style.fontSize = `${input.value}px`;
+
 
 input.addEventListener('input', onSize);
 
